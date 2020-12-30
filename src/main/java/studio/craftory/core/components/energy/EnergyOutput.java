e9b8ab83b7225a16d00ebdcc0c89da52f4181aa4
Brett Saunders <brett_saunders@btinternet.com>
2020-12-30 00:29:27 +0000

    Added Energy Components

---
+++ src/main/java/studio/craftory/core/components/energy/EnergyOutput.java
@@ -0,0 +1,41 @@
+package studio.craftory.core.components.energy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import lombok.NonNull;
+import studio.craftory.core.utils.Reflections;
+
+/**
+ * Energy Output Component
+ */
+public interface EnergyOutput extends EnergyStorage {
+
+  /**
+   * Extracts energy from objects storage up to the maximum amount requested.
+   * @param energyRequested Maximum amount of energy to extract
+   * @return Amount of energy extracted
+   */
+  default long extractEnergy(@NonNull final long energyRequested) {
+    synchronized (this) {
+      long energyOutputted = Math.min(getEnergyStored(), Math.min(getMaxEnergyOutput(), energyRequested));
+      decreaseStoredEnergy(energyOutputted);
+      return energyOutputted;
+    }
+  }
+
+  /**
+   * @return Maximum amount of energy object is capable of outputting
+   */
+  default long getMaxEnergyOutput() {
+    return Reflections.getClassAnnotation(this, EnergyOutputData.class).maxExtract();
+  }
+
+  //Annotations
+  @Target(ElementType.TYPE)
+  @Retention(RetentionPolicy.RUNTIME)
+  @interface EnergyOutputData {
+    int maxExtract();
+  }
+}
--- src/main/java/studio/craftory/core/data/SafePlugin.java
+++ src/main/java/studio/craftory/core/data/SafePlugin.java
@@ -1,4 +1,4 @@
-package studio.craftory.core.data_containers;
+package studio.craftory.core.data;
 
 import java.io.Serializable;
 import java.util.Optional;
