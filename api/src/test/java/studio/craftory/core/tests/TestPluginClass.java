267cc5caff22627f42b9c058b1982e7a48632c7e
Brett Saunders <brett_saunders@btinternet.com>
2021-01-27 22:57:01 +0000

    test(setup): Added Junit, MockBukkit and Basic Unit Test

---
+++ api/src/test/java/studio/craftory/core/tests/TestPluginClass.java
@@ -0,0 +1,32 @@
+package studio.craftory.core.tests;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import studio.craftory.core.Craftory;
+
+public class TestPluginClass {
+
+  private static Craftory plugin;
+
+  @BeforeAll
+  public static void load() {
+    MockBukkit.mock();
+    plugin = MockBukkit.load(Craftory.class);
+  }
+
+  @AfterAll
+  public static void unload() {
+    MockBukkit.unmock();
+  }
+
+  @Test
+  @DisplayName("Test plugin getters return a value")
+  void testGetters() {
+    Assertions.assertNotNull(Craftory.getInstance());
+    Assertions.assertNotNull(Craftory.getInstance().getPersistenceManager());
+  }
+}
