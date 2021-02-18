ee48ad979c38bfe7ffc802f78d460986586c3f67
Brett Saunders <brett_saunders@btinternet.com>
2021-02-18 23:52:03 +0000

    Resource pack copying

--- api/src/main/java/studio/craftory/core/CraftorySetup.java
+++ api/src/main/java/studio/craftory/core/CraftorySetup.java
@@ -1,5 +1,8 @@
 package studio.craftory.core;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,6 +12,11 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Optional;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import org.bukkit.Bukkit;
@@ -17,6 +25,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import studio.craftory.core.utils.Log;
 
 public class CraftorySetup extends BukkitRunnable {
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   public void run() {
@@ -37,6 +47,11 @@ public class CraftorySetup extends BukkitRunnable {
         }
         downloadResource(((CraftoryAddon) plugin).getAddonResources(), zipFile);
         unZip(zipFile, destDir);
+
+        File endDir = new File(Craftory.getInstance().getDataFolder(), "resourcepacks");
+        copyResources(destDir.getAbsolutePath(), endDir.getAbsolutePath());
+        zipFile.delete();
+        destDir.delete();
       }
     }
   }
@@ -49,6 +64,85 @@ public class CraftorySetup extends BukkitRunnable {
     } catch (IOException fileNotFoundException) {
       fileNotFoundException.printStackTrace();
     }
+  }
+
+  private void copyResources(String sourceDirectoryLocation, String destinationDirectoryLocation)  {
+    try {
+      Files.walk(Paths.get(sourceDirectoryLocation))
+           .forEach(source -> {
+             Path destination = Paths.get(destinationDirectoryLocation, source.toString().substring(sourceDirectoryLocation.length()));
+
+             if (Files.exists(destination) && !Files.isDirectory(destination)) {
+               mergeResources(source.toFile(), destination.toFile());
+             } else {
+               try {
+                 Files.copy(source, destination);
+               } catch (IOException e) {
+                 e.printStackTrace();
+               }
+             }
+           });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void mergeResources(File source, File dest) {
+    Optional<String> fileExtensionOptional = getExtension(dest.getName());
+    if (!fileExtensionOptional.isPresent()) {
+      Log.warn("What is this");
+      return;
+    }
+
+    switch (fileExtensionOptional.get()) {
+      case "png":
+      case "ogg":
+      case "mcmeta":
+        Log.warn("Can't merge file " + dest.getName());
+        break;
+      case "json":
+        mergeJSON(source, dest);
+        break;
+    }
+  }
+
+  private void mergeJSON(File source, File dest) {
+    try {
+      JsonNode sourceNode = objectMapper.readTree(source);
+      JsonNode destNode = objectMapper.readTree(dest);
+      objectMapper.writeValue(dest, merge(destNode, sourceNode));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private JsonNode merge(JsonNode mainNode, JsonNode updateNode) {
+    Iterator<String> fieldNames = updateNode.fieldNames();
+    while (fieldNames.hasNext()) {
+
+      String fieldName = fieldNames.next();
+      JsonNode jsonNode = mainNode.get(fieldName);
+      // if field exists and is an embedded object
+      if (jsonNode != null && jsonNode.isObject()) {
+        merge(jsonNode, updateNode.get(fieldName));
+      }
+      else {
+        if (mainNode instanceof ObjectNode) {
+          // Overwrite field
+          JsonNode value = updateNode.get(fieldName);
+          ((ObjectNode) mainNode).set(fieldName, value);
+        }
+      }
+
+    }
+
+    return mainNode;
+  }
+
+  private Optional<String> getExtension(String filename) {
+    return Optional.ofNullable(filename)
+                   .filter(f -> f.contains("."))
+                   .map(f -> f.substring(filename.lastIndexOf(".") + 1));
   }
 
   private void unZip(File input, File destDir)  {
